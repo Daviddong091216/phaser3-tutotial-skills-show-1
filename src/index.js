146be3e7d5ec05f8cd3ad03674eb9b3acea76e9a
David Dong <daviddong201712@gmail.com>
2021-01-24 17:35:48 -0600

    adds platforms variable and create plateform.

--- src/index.js
+++ src/index.js
@@ -22,7 +22,16 @@ class MyGame extends Phaser.Scene {
     }
 
     create() {
+        // this.add.image(400, 300, 'star');
         this.add.image(400, 300, 'sky');
+
+        platforms = this.physics.add.staticGroup();
+
+        platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+        platforms.create(600, 400, 'ground');
+        platforms.create(50, 250, 'ground');
+        platforms.create(750, 220, 'ground');
     }
 
     update() {
@@ -45,5 +54,6 @@ const config = {
     scene: MyGame
 };
 
+var platforms;
 
 const game = new Phaser.Game(config);
